c0fbf3f0329138629f79512db2d1f1407994fa73
roguib <rgrguib@gmail.com>
2022-12-04 23:21:41 +0100

    Base commit for ExpressionStatement.java

---
+++ src/main/java/org/interpreter/ast/ExpressionStatement.java
@@ -0,0 +1,27 @@
+package org.interpreter.ast;
+
+import org.interpreter.lexer.Token;
+
+public class ExpressionStatement implements Statement {
+    private Token token;
+    private Expression expression;
+
+
+    @Override
+    public String tokenLiteral() {
+        return token.getLiteral();
+    }
+
+    @Override
+    public void statementNode() {
+
+    }
+
+    @Override
+    public String toString() {
+        if (expression != null) {
+            return expression.toString();
+        }
+        return "";
+    }
+}
