1406934e05a829dde0d5e3c54aa8f654e753c501
roguib <rgrguib@gmail.com>
2023-11-11 20:27:16 +0100

    listen for changes in MonacoEditor and pass data to Playground

--- web/playground/src/components/editor/Editor.js
+++ web/playground/src/components/editor/Editor.js
@@ -1,16 +1,47 @@
+import { useState } from "react";
 import { default as MonacoEditor } from "@monaco-editor/react";
-
 import "./Editor.css";
 
-function Editor() {
+
+/**
+ *
+ * @param {Object} props
+ * @param {Function} onEditorChanged A callback function that expects the current value of the
+ * program written in the MonacoEditor
+ * @returns
+ */
+function Editor({ onEditorChanged }) {
+  const MS_TIMEOUT_TO_EVALUATE = 5000;
+  const [lastModifiedTimeout, setLastModifiedTimeout] = useState(() => {});
+
+  /**
+   * Sets a timeout that will pass the latest modification to the parent component
+   * if the latest modification was made MS_TIMEOUT_TO_EVALUATE or more. If two
+   * or more modifications are made between 0..MS_TIMEOUT_TO_EVALUATE then it clears
+   * the timeout and sets another one with the updated value
+   *
+   * @param {string} program
+   */
+  const handleEditorChange = (program) => {
+    if (lastModifiedTimeout) {
+      clearTimeout(lastModifiedTimeout);
+    }
+
+    setLastModifiedTimeout(
+      setTimeout(() => {
+        console.log(program);
+        console.log(`${MS_TIMEOUT_TO_EVALUATE} without changes, sending the back to parent component`);
+        onEditorChanged(program);
+      }, MS_TIMEOUT_TO_EVALUATE));
+  };
 
   return (
     <div className="Editor">
-      <p>This is the Editor area</p>
       <MonacoEditor
         height="90vh"
         defaultLanguage="javascript"
         defaultValue="// some comment"
+        onChange={handleEditorChange}
       />
     </div>
   );
--- web/playground/src/screens/playground/Playground.js
+++ web/playground/src/screens/playground/Playground.js
@@ -4,9 +4,20 @@ import "./Playground.css";
 
 function Playground() {
 
+  /**
+   * A function that sends the latest program to the server and waits
+   * for an evaluation (TBD)
+   * @param {*} program
+   */
+  const handleEditorChanged = (program) => {
+    console.log(program);
+  };
+
   return (
     <div className="Playground" data-testid="playground-screen">
-      <Editor />
+      <Editor
+        onEditorChanged={handleEditorChanged}
+      />
     </div>
   );
 }
