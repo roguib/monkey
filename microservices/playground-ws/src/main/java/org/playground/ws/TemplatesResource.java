de588b99265dcedaa931f1f68b19e4343d2e71e6
roguib <rgrguib@gmail.com>
2024-02-25 18:49:28 +0100

    cleanup TemplatesResource.java

--- microservices/playground-ws/src/main/java/org/playground/ws/TemplatesResource.java
+++ microservices/playground-ws/src/main/java/org/playground/ws/TemplatesResource.java
@@ -3,11 +3,9 @@ package org.playground.ws;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import org.playground.ws.dao.TemplateDao;
 import org.playground.ws.dto.TemplateDto;
 import org.playground.ws.repository.TemplateRepository;
 
@@ -19,18 +17,6 @@ import java.util.List;
 public class TemplatesResource {
     @Inject
     private TemplateRepository templateRepository;
-
-    // TODO: Delete me
-    @POST
-    @Path("/new")
-    @Produces(MediaType.APPLICATION_JSON)
-    public HashMap createTemplate() {
-        final TemplateDao p = TemplateDao.of("TemplateDao title", "TemplateDao description", "let a = 1;");
-        templateRepository.save(p);
-        final HashMap<String, String> resp = new HashMap<>();
-        resp.put("created", "ok");
-        return resp;
-    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
