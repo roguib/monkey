c51ee3dbe52c9e08f65d485ffb94227078aa3d0c
roguib <rgrguib@gmail.com>
2024-03-03 16:45:47 +0100

    init templates from a dataset

--- microservices/playground-ws/scripts/init.sql
+++ microservices/playground-ws/scripts/init.sql
@@ -1,6 +1,39 @@
 CREATE TABLE templates (
     id VARCHAR(255) PRIMARY KEY,
-    description TEXT,
     title VARCHAR(255),
+    description TEXT,
     program TEXT
 );
+
+INSERT INTO templates VALUES('variableBinding', 'Variable binding', 'How variable binding works in Monkey language', '');
+UPDATE templates
+SET program = (
+    SELECT program
+    FROM (
+        SELECT *
+        FROM pg_read_file('/data/variable-binding-template.txt')
+    ) AS data(program)
+)
+WHERE id = 'variableBinding';
+
+INSERT INTO templates VALUES('controlFlow', 'Control flow', 'Control the execution flow of a Monkey program', '');
+UPDATE templates
+SET program = (
+    SELECT program
+    FROM (
+        SELECT *
+        FROM pg_read_file('/data/control-flow-template.txt')
+    ) AS data(program)
+)
+WHERE id = 'controlFlow';
+
+INSERT INTO templates VALUES('closures', 'Closures', 'Closures in Monkey language', '');
+UPDATE templates
+SET program = (
+    SELECT program
+    FROM (
+        SELECT *
+        FROM pg_read_file('/data/closures-template.txt')
+    ) AS data(program)
+)
+WHERE id = 'closures';
--- web/playground/src/App.js
+++ web/playground/src/App.js
@@ -25,7 +25,7 @@ function App() {
     }
     const { id } = await data.json();
     console.log(`new playground created with id ${id}`);
-    navigate(`/playground/${id}`, { state: { skipFetchHistory: true } });
+    navigate(`/playground/${id}`, { state: { skipFetchHistory: !templateId ? true : false } });
   };
 
   return (
