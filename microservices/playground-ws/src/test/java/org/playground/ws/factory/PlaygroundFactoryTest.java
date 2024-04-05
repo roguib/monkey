2efe5e9ca7bc6cf1dcf02033cdf4feae55bcdde0
roguib <rgrguib@gmail.com>
2024-04-05 19:44:46 +0200

    better fix for not being able to run unit tests

--- microservices/playground-ws/src/test/java/org/playground/ws/factory/PlaygroundFactoryTest.java
+++ microservices/playground-ws/src/test/java/org/playground/ws/factory/PlaygroundFactoryTest.java
@@ -26,7 +26,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
-@HelidonTest
 public class PlaygroundFactoryTest {
 
     @Mock
