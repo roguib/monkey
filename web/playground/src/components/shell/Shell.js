7a3fff515a0687e7e326939c109c7803c584767a
roguib <rgrguib@gmail.com>
2024-07-13 10:54:22 +0200

    sort history in client

--- web/playground/src/components/shell/Shell.js
+++ web/playground/src/components/shell/Shell.js
@@ -5,19 +5,21 @@ import { memo } from "react";
 /**
  * 
  * @param {Object} props 
- * @param {string[]} props.evalResults Evaluation results that have been generated
+ * @param {Array<{date: string, result: string}>} props.evalResults Evaluation results that have been generated
  * during the lifecycle of this playground
  */
-const Shell = memo(function Shell({ evalResults }) {
+const Shell = memo(function Shell({ evalResults = [] }) {
   return (
     <div data-testid="shell">
       <ListGroup>
         {
-          evalResults?.map((result, i) => (
-            // eslint complains missing key
-            // eslint-disable-next-line
-            <ListGroup.Item eventKey={i} data-testid="shell-result">{result}</ListGroup.Item>
-          ))
+          evalResults
+            .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+            .map((result, i) => (
+              // eslint complains missing key
+              // eslint-disable-next-line
+              <ListGroup.Item eventKey={i} data-testid="shell-result">{result.result}</ListGroup.Item>
+            ))
         }
       </ListGroup>
     </div>
