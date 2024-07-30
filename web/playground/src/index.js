59bbb02ac5399d89c40368b88a8ae14567fc58b8
roguib <rgrguib@gmail.com>
2024-07-30 17:20:54 +0200

    handle 404 errors while navigating

--- web/playground/src/index.js
+++ web/playground/src/index.js
@@ -8,17 +8,35 @@ import "./index.scss";
 import App from "./App";
 import Playground from "./screens/playground/Playground";
 import { Configuration } from "./Configuration.js";
+import ErrorBoundary from "./components/errorBoundary/ErrorBoundary";
+import Placeholder from "./components/placeholder/Placeholder";
 
 const url = Configuration.path;
+
+const getErrorElement = () => {
+  return (
+    <ErrorBoundary
+      fallback={
+        <Placeholder
+          type="page"
+          title="Ops! 404 Not found"
+          description="The page you're looking for doesn't exist." />
+      }
+    />
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: `${url}`,
     element: <App />,
+    errorElement: getErrorElement()
   },
   {
     path: `${url}:playgroundId`,
-    element: <Playground />
+    element: <Playground />,
+    errorElement: getErrorElement(),
+    // TODO: loader function that checks if the playground exist before navigating
   }
 ]);
 
